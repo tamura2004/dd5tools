9f5dc686faaf074e68e2d6da31e1c618c1f1112d
tamura <tamura2004@gmail.com>
2018-12-23 13:11:46 +0900

    Deploy

---
+++ docs/precache-manifest.a6870ff3d9f5369276fd73c63d1c2d8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "67edd206a716fa3083d3",
+    "url": "/dd5tools/css/app.a1936567.css"
+  },
+  {
+    "revision": "67edd206a716fa3083d3",
+    "url": "/dd5tools/js/app.4e0393e2.js"
+  },
+  {
+    "revision": "a2d432f10fed3c992c22",
+    "url": "/dd5tools/css/chunk-vendors.71489c59.css"
+  },
+  {
+    "revision": "a2d432f10fed3c992c22",
+    "url": "/dd5tools/js/chunk-vendors.85f992e2.js"
+  },
+  {
+    "revision": "de106d44fe75f1793955dba15edf4cd6",
+    "url": "/dd5tools/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/dd5tools/robots.txt"
+  }
+];
---
+++ vue.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  baseUrl: process.env.NODE_ENV === 'production'
+    ? '/dd5tools/'
+    : '/',
+  outputDir: 'docs',
+}
