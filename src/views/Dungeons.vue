59a7b24e2183263cb0ab9d81208935abd44e3b60
M.Tamura <tamura2004@gmail.com>
2019-02-07 13:21:43 +0900

    Change players menu bar

--- src/views/Dungeons.vue
+++ src/views/Dungeons.vue
@@ -1,20 +1,26 @@
 <template lang="pug">
   v-app
-    v-toolbar
+    v-toolbar(app)
       v-btn(icon to="/"): v-icon clear
       v-toolbar-title.text-xs-center ダンジョン
-      v-btn(absolute dark fab bottom right color="red" @click="newDungeon")
-        v-icon add
-    v-list(two-line)
-      div(v-for="(dungeon, n) in dungeons")
-        v-list-tile.my-1.elevation-4
-          v-list-tile-avater(tile)
-            v-btn(fab dark small color="grey") {{ n }}
-          v-list-tile-content.ml-2
-            v-list-tile-title {{ dungeon.name }}
-            v-list-tile-sub-title {{ dungeon.memo }}
-          v-list-actions
-            v-icon keyboard_arrow_right
+      v-spacer
+      v-btn(icon @click="extended = !extended"): v-icon search
+      v-btn(icon to="/"): v-icon add
+    v-toolbar(v-show="extended")
+      v-btn(icon): v-icon clear
+      v-toolbar-title two
+    v-content
+      v-container(:fluid="!$vuetify.breakpoint.xsOnly")
+        v-list(two-line)
+          div(v-for="(dungeon, n) in dungeons")
+            v-list-tile.my-1.elevation-4
+              v-list-tile-avater(tile)
+                v-btn(fab dark small color="grey") {{ n }}
+              v-list-tile-content.ml-2
+                v-list-tile-title {{ dungeon.name }}
+                v-list-tile-sub-title {{ dungeon.memo }}
+              v-list-actions
+                v-icon keyboard_arrow_right
 </template>
 
 <script lang="ts">
@@ -22,6 +28,7 @@
 
   @Component
   export default class Dungeons extends Vue {
+    private extended: boolean = false;
     private dungeons = [
       {
         name: '峠のダンジョン',
