85f32493e5228c88b76051d57b41294d1fb2b295
M.Tamura <tamura2004@gmail.com>
2019-02-25 07:58:20 +0900

    Fix bug

--- src/App.vue
+++ src/App.vue
@@ -5,28 +5,20 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import Menu from '@/views/Menu.vue';
 
-@Component({
-  components: { Menu },
-})
+@Component
 export default class App extends Vue {
-
-
-  private home(): void {
-    this.$router.push('/');
-  }
 }
 </script>
 
 <style lang="stylus">
   .router-enter-active, .router-leave-active
     transition all 0.3s ease
-  
+
   .router-enter
     transform translateX(100%)
     // opacity 0
-  
+
   .router-leave-active
     // opacity 0
     transform translateX(-100%)
