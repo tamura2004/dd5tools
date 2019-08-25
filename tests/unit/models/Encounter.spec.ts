dd4b696838f0022cc953c31da0877b466a50a043
M.Tamura <tamura2004@gmail.com>
2019-08-25 12:00:58 +0900

    Remove old folder

--- tests/unit/models/Encounter.spec.ts
+++
@@ -1,8 +0,0 @@
-import Encounter from '@/models/Encounter';
-
-describe('Encounter.ts', () => {
-  it('is initialized partial', () => {
-    const log = new Encounter({ level: 0 });
-    expect(log.sessionId).toBe(null);
-  });
-});
--- tests/unit/models/Session.spec.ts
+++
@@ -1,9 +0,0 @@
-import Session from '@/models/Session';
-
-describe('Session.ts', () => {
-  it('is initialized partial', () => {
-    const session = new Session({ name: 'NAME' });
-    expect(session.dungeonId).toBe(null);
-    expect(session.npcId).toBe(null);
-  });
-});
