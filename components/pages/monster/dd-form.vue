945cb786456ab1c4a5fa5a44e782d1099ea706b2
M.Tamura <tamura2004@gmail.com>
2019-08-28 17:15:45 +0900

    Add monster form

--- components/pages/monster/dd-form.vue
+++ components/pages/monster/dd-form.vue
@@ -4,11 +4,29 @@
     v-divider
     v-card-text
       v-form
-        v-text-field(label="名前" :value="data.name" @input="name($event)")
+        v-text-field(label="名称" :value="data.name" @input="name($event)")
+        v-text-field(label="サイズ" :value="data.size" @input="size($event)")
+        v-text-field(label="種別" :value="data.type" @input="type($event)")
+        v-text-field(label="属性" :value="data.alignment" @input="alignment($event)")
+        v-text-field(label="AC" type="number" :value="data.ac" @input="ac($event)")
+        v-text-field(label="hp" type="number" :value="data.hp" @input="hp($event)")
+        v-text-field(label="移動" :value="data.mv" @input="mv($event)")
+        v-text-field(label="exp" type="number" :value="data.exp" @input="exp($event)")
+        dd-array-field(:value="data.ability")
         v-textarea(
           label="属性"
           :value="data.attributes.join('\\n')"
           @input="attributes($event.split('\\n'))"
+        )
+        v-textarea(
+          label="アクション"
+          :value="data.actions.join('\\n')"
+          @input="actions($event.split('\\n'))"
+        )
+        v-textarea(
+          label="特殊能力"
+          :value="data.specials.join('\\n')"
+          @input="specials($event.split('\\n'))"
         )
     v-card-actions
       v-btn(color="primary" @click="$emit('click')") 登録
@@ -16,11 +34,28 @@
 
 <script>
 import { mapGetters, mapActions } from "vuex";
+import ddArrayField from "~/components/ui/dd-array-field"
 
 export default {
+  components: {
+    ddArrayField,
+  },
   props: ["title"],
   computed: mapGetters("form/monster", ["data"]),
-  methods: mapActions("form/monster", ["name", "attributes"]),
+  methods: mapActions("form/monster", [
+    "name",
+    "size",
+    "type",
+    "alignment",
+    "ac",
+    "hp",
+    "mv",
+    "ability",
+    "exp",
+    "attributes",
+    "actions",
+    "specials",
+  ]),
 };
 </script>
 
---
+++ components/ui/dd-array-field.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  v-row
+    v-col(v-for="i in 6")
+      v-text-field(:label="label(i)" :value="value && value[i-1]")
+</template>
+
+<script>
+export default {
+  props: ["value"],
+  methods: {
+    label(i) {
+      const labels = ["str", "dex", "con", "int", "wis", "cha"];
+      return labels[i - 1];
+    },
+  },
+};
+</script>
