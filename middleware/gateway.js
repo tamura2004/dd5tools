d5755bdbe655b0a869a3857ddf5d54244c47ada4
tamura <tamura2004@gmail.com>
2019-09-21 15:04:05 +0900

    Update sample

--- middleware/gateway.js
+++ middleware/gateway.js
@@ -3,7 +3,7 @@ import WEAPON_DATA from "~/assets/data/weapons";
 
 export default ({ store }) => {
   // store.dispatch("monsters/listen");
-  store.dispatch("places/init", PLACE_DATA);
+  // store.dispatch("places/init", PLACE_DATA);
   store.dispatch("weapons/init", WEAPON_DATA);
   // store.dispatch("players/listen")
   // store.dispatch("guilds/listen")
--- store/places.js
+++ store/places.js
@@ -1,5 +1,6 @@
 import { Collection } from "~/plugins/collection";
-import { IndexedDB } from "~/plugins/indexeddb";
+// import { IndexedDB } from "~/plugins/indexeddb";
+import PLACE_DATA from "~/assets/data/places";
 
-export const { state, getters, mutations } = new Collection("places");
-export const { actions } = new IndexedDB("places");
+export const { state, getters, mutations } = new Collection("places", PLACE_DATA);
+// export const { actions } = new IndexedDB("places");
